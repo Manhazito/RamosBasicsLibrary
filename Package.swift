a7b750ce2efbd630f42765df8541372d51872e95
Filipe Ramos <pandinus.imperator@gmail.com>
2024-07-14 12:49:35 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RamosBasicsLibrary",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "RamosBasicsLibrary",
+            targets: ["RamosBasicsLibrary"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "RamosBasicsLibrary"),
+        .testTarget(
+            name: "RamosBasicsLibraryTests",
+            dependencies: ["RamosBasicsLibrary"]),
+    ]
+)
